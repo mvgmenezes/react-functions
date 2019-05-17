f90d5834bc9bba6178f4ba7c68f33773bb57129e
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-17 16:56:29 -0300

    Usando children dentro dos componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Square from './square';
 import Title from './title';
+import Button from './button';
 
 class App extends React.Component {
 
   clicouNaDiv(){
     console.log('Clicou');
-    alert('Clicou');
+    //alert('Clicou');
   }
 
 
@@ -15,6 +16,13 @@ class App extends React.Component {
         
         <div className='container' onClick={this.clicouNaDiv}>
           <Title name="Marcus" />
+          
+          <Button>Teste</Button>
+
+          <Button>
+            <span>Teste com children </span>
+          </Button>
+
 
           {['green', 'yellow', 'blue', 'green'].map( (color, index) => 
             <Square key={index} color={color} />
