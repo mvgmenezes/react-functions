fd0290324921d01e25dd2f3717ff0209a515472e
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-17 15:32:12 -0300

    Trabalhando com array com map e index para nao duplicar a key

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends React.Component {
   render(){
       return (
         <div className='container'>
-          {['green', 'yellow', 'blue'].map( (color) => 
-            <Square key={color} color={color} />
+          {['green', 'yellow', 'blue', 'green'].map( (color, index) => 
+            <Square key={index} color={color} />
           )}
         </div>
       );
