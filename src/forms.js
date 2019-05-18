e84fa3e47692834cc5e6e9c2c85509ac4dd68d4e
Marcus Menezes <mvgmenezes@gmail.com>
2019-05-18 11:55:34 -0300

    Formulario - Select

--- src/forms.js
+++ src/forms.js
@@ -6,7 +6,8 @@ class Forms extends Component {
         this.state = {
             value: 'Input Controlled',
             checked: false,
-            radio: false
+            radio: false,
+            select: '2'
         }
         //bind no final para informar que o this dentor da funcao é o this da classe Forms
         this.entrouDadosNoInput = this.entrouDadosNoInput.bind(this);
@@ -41,7 +42,24 @@ class Forms extends Component {
                         <input type="checkbox" defaultChecked='false'/>Checkbox uncontrolled
                     </label>
 
-                    <br />
+                    <br/>
+                    <h3>Select Controlled - trabalho com state</h3>
+
+                    <select value={this.state.select} onChange={(e) => {
+                        this.setState({select: e.target.value})}}>
+
+                        <option value='1'>Opção 1</option>
+                        <option value='2'>Opção 2</option>
+                        <option value='3'>Opção 3</option>
+                    </select>
+                    <br/>
+                    
+                    <h3>Select Uncontrolled - NAO trabalho com o state, uso o default do react</h3>
+                    <select multiple>
+                        <option value='1'>Opção 1</option>
+                        <option value='2'>Opção 2</option>
+                        <option value='3'>Opção 3</option>
+                    </select>
                     
                 </form>
             </div>
